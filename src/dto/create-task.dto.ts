4c052358337cdb344519afd4a6579070d1e9b084
jemprojects <jemprojectsgit@gmail.com>
2023-11-12 18:44:40 -0300

    Con class-validator

--- src/dto/create-task.dto.ts
+++ src/dto/create-task.dto.ts
@@ -1,5 +1,14 @@
-export interface CreatedTaskDto {
+import { IsString, IsBoolean, IsOptional, IsNotEmpty } from 'class-validator';
+export class CreatedTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   done?: boolean;
 }
--- src/dto/update-task.dto.ts
+++ src/dto/update-task.dto.ts
@@ -1,6 +1,15 @@
 // el signo de pregunta es para decirle que puede que venga o no
-export interface UpdateTaskDto {
+import { IsString, IsBoolean, IsOptional } from 'class-validator';
+export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
+  @IsOptional()
   done?: boolean;
 }
